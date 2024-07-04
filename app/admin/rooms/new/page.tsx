2dd774211f78e02df1eb0dd103aa8cf295e64ed7
lc <codinghistoryoflance@gmail.com>
2024-07-05 00:03:19 +0800

    remove rooms

--- app/admin/rooms/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import NewRoom from "@/components/admin/NewRoom";
-
-export const metadata = {
-  title: "Create New Room - ADMIN",
-};
-
-export default async function NewRoomPage() {
-  return <NewRoom />;
-}
